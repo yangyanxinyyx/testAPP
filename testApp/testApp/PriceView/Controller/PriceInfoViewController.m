34b44902e51ce269d2da1a98fbfd909f0b5de97e
yyx <13727634817@163.com>
2018-03-16 20:34:43 +0800

    报价详情

---
+++ testApp/testApp/PriceView/Controller/PriceInfoViewController.m
@@ -0,0 +1,37 @@
+//
+//  PriceInfoViewController.m
+//  testApp
+//
+//  Created by 严玉鑫 on 2018/3/16.
+//  Copyright © 2018年 outPutTeam. All rights reserved.
+//
+
+#import "PriceInfoViewController.h"
+
+@interface PriceInfoViewController ()
+
+@end
+
+@implementation PriceInfoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
