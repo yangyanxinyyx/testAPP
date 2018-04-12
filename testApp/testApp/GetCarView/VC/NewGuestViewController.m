5373a5cf0f240203fa1abaf2788e83c1b7709225
yanxin_yang <yangyx@adnonstop.com>
2018-04-12 23:00:17 +0800

    提交

--- testApp/testApp/GetCarView/VC/NewGuestViewController.m
+++ testApp/testApp/GetCarView/VC/NewGuestViewController.m
@@ -8,15 +8,50 @@
 
 #import "NewGuestViewController.h"
 
-@interface NewGuestViewController ()
+@interface NewGuestViewController ()<BaseNavigationBarDelegate>
+
+@property (nonatomic,assign) BOOL isOrder;
 
 @end
 
 @implementation NewGuestViewController
 
+- (instancetype)initWithType:(BOOL)isOrder
+{
+    if (self = [super init]) {
+        self.isOrder = isOrder;
+
+    }
+    return self;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    BaseNavigationBar *topBar = [[BaseNavigationBar alloc] init];
+    topBar.delegate  = self;
+    topBar.title = self.isOrder ? @"新增订单" : @"新增维修";
+    [self.view addSubview:topBar];
+
+    self.view.backgroundColor = COLOR_RGB_255(242, 242, 242);
+
+    []
+
     // Do any additional setup after loading the view.
+}
+
+- (void)baseNavigationDidPressCancelBtn:(BOOL)isCancel
+{
+    if (isCancel) {
+        [self.navigationController popViewControllerAnimated:YES];
+    }
+}
+
+- (void)createUI
+{
+    for (int i=0; i<; <#increment#>) {
+        <#statements#>
+    }
+
 }
 
 - (void)didReceiveMemoryWarning {
