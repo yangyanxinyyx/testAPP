88caf5587a6df7c38e76ec5db63fdbabb52adffe
yyx <13727634817@163.com>
2018-04-25 23:21:27 +0800

    引导图和 启动图

--- testApp/testApp/AppDelegate.m
+++ testApp/testApp/AppDelegate.m
@@ -13,8 +13,13 @@
 #import "UserViewController.h"
 #import "BaseTabbarController.h"
 #import <AMapFoundationKit/AMapFoundationKit.h>
-@interface AppDelegate ()<UITabBarControllerDelegate>
+/**当前app版本号*/
+#define BC_AppCurrentBuildVerison @"CurrentBuildVersion"
+@interface AppDelegate ()<UIScrollViewDelegate,UITabBarControllerDelegate>
+@property (nonatomic, copy)UIView *bgView;
 
+@property (nonatomic, retain)UIScrollView *myScrollView;
+@property (nonatomic, copy)NSArray *imageArray;
 @end
 
 @implementation AppDelegate
@@ -27,14 +32,62 @@
     [self.window makeKeyAndVisible];
     
     [AMapServices sharedServices].apiKey = @"780fe25c204479d57fd155664e193fa6";
-
     BaseTabbarController *tab = [[BaseTabbarController alloc] init];
     tab.delegate = self;
     self.window.rootViewController = tab;
+    
+    NSString *currentBuildVersion = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];
+    NSString *defaultsBuildVersion = [[NSUserDefaults standardUserDefaults] objectForKey:BC_AppCurrentBuildVerison];
+    if ([currentBuildVersion isEqualToString:defaultsBuildVersion]) {
+        [[NSUserDefaults standardUserDefaults] setObject:currentBuildVersion forKey:BC_AppCurrentBuildVerison];
+        self.imageArray = [NSArray arrayWithObjects:[UIImage imageNamed:@"guide1"],[UIImage imageNamed:@"guide2"],[UIImage imageNamed:@"guide3"],nil];
+        self.myScrollView.contentSize = CGSizeMake(SCREEN_WIDTH * 3, 0);
+        [self.bgView addSubview:self.myScrollView];
+        [self.window addSubview:self.bgView];
+        for (int i = 0; i < self.imageArray.count; i++) {
+            UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(self.myScrollView.frame.size.width*i, 0, self.myScrollView.frame.size.width ,self.myScrollView.frame.size.height)];
+            imageView.userInteractionEnabled = YES;
+            imageView.image = [self.imageArray objectAtIndex:i];
+            [self.myScrollView addSubview:imageView];
+            
+            if (i==2) {
+                
+                UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake(self.myScrollView.frame.size.width*2, self.myScrollView.frame.size.height-75, SCREEN_WIDTH ,150)];
+                
+                [btn addTarget:self action:@selector(closeView) forControlEvents:UIControlEventTouchUpInside];
+                [self.myScrollView addSubview:btn];
+            }
+        }
+    } else {
+        
+    }
 
     return YES;
 }
 
+- (void)closeView {
+    
+    [self.bgView removeFromSuperview];
+}
+- (UIScrollView *)myScrollView{
+    if (!_myScrollView) {
+        _myScrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)];
+        _myScrollView.pagingEnabled = YES;
+        _myScrollView.showsHorizontalScrollIndicator =  NO;
+        _myScrollView.backgroundColor = [UIColor blackColor];
+        _myScrollView.delegate = self;
+        
+    }
+    return _myScrollView;
+}
+
+- (UIView *)bgView{
+    if (!_bgView) {
+        _bgView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)];
+        _bgView.backgroundColor = [UIColor clearColor];
+    }
+    return _bgView;
+}
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
